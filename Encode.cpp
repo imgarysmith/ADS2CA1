dde763c496d3c60559ca751309e5abd3c999adad
Gary Smith <gary@imgarysmith.com>
2014-03-24 11:42:24 +0000

    Frequency Map

--- Encode.cpp
+++ Encode.cpp
@@ -3,3 +3,45 @@
 // Gary Francis Smith - X00080112
 
 #include "Encode.h"
+#include <map>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+// Constructor
+Encode::Encode() {
+
+};
+
+// Take input from file and count the frequency of each letter.
+// Bool return true if file exists, returns false if file doesn't exist.
+void Encode::countFrequency() {
+  string inputText;
+  ifstream inFile;
+
+  // Open File
+  inFile.open("test.txt");
+  // Check file exists
+  if (!inFile) {
+    cout << "Can't open file.";
+    //return false;
+  }
+
+  // While text still in file
+  while (inFile >> inputText) {
+    // For every character in the word
+    for (char& nextChar : inputText) {
+      freqMap[nextChar] += 1;
+    }
+  }
+
+  // Print out frequencies
+  for (int i = 0; i < freqMap.size(); i++) {
+    cout << freqMap[i] << endl;
+  }
+
+  // Close
+  inFile.close();
+
+  //return true;
+};
--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 // Chris Nicholl - X00073131
 // Gary Francis Smith - X00080112
 
+#include "Encode.h"
 #include <iostream>
 using namespace std;
 
+void countFrequency();
+
 int main() {
+
+  Encode encodeFile = *new Encode();
+  encodeFile.countFrequency();
 
   return 0;
 }
