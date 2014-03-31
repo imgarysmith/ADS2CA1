c3d6f61a1f46be14a3a325352c0b5f9521ffa8ad
ChrisNicholl <chrisnicholl@gmail.com>
2014-03-31 17:42:57 +0100

    Caeser Cypher
    Referenced and working (double check please)

---
+++ Caesar.cpp
@@ -0,0 +1,67 @@
+//Gary Smith
+//Chris Nicholl
+/***************************************************************************************
+*    Title: <title of program/source code>
+*    Author: <author(s) names>
+*    Date: <date>
+*    Code version: <code version>
+*    Availability: http://rosettacode.org/wiki/Caesar_cipher#C.2B.2B
+ 
+*
+***************************************************************************************/
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+
+using namespace std;
+
+class Caeser{
+private:
+	int shift;
+public:
+	Caeser(int s):shift(s){}
+	char operator()(char c)
+	{
+		if (isspace(c))
+			return ' ';
+		else{
+			static string letters("abcdefghijklmnopqrstuvwxyz");
+			string::size_type found = letters.find(tolower(c));
+
+			int shiftedPosition = (static_cast<int>(found)+shift) % 26;
+			if(shiftedPosition<0)
+				shiftedPosition=26+shiftedPosition;
+			char shifted=letters[shiftedPosition];
+			return shifted;
+		}
+	}
+};
+
+int main(){
+	int shifts = 0;
+
+	ifstream ifs("input.txt");
+	string input((istreambuf_iterator<char>(ifs)),
+		(std::istreambuf_iterator<char>()));
+	
+	cout << "Shift letters how many spaces?\n";
+	cin >> shifts;
+
+	cout << "No Encryption:\n" << input << endl;
+	transform(input.begin(), input.end(), input.begin(),
+
+	Caeser(shifts));
+	cout <<endl<< "Caeser Encryption:\n" << endl << endl;
+	cout << input<< endl;
+	shifts *= -1;
+
+	transform(input.begin(), input.end(), input.begin(),
+	Caeser(shifts));
+	cout << endl<<"Decrypted:\n"<<endl <<endl;
+
+	cout << input << endl;
+	system("PAUSE");
+	return 0;
+}
